ac470579569aaa6bf0420b24a81fd897bfa85938
Valerii-Postrybailo <postribailo.valeri@gmail.com>
2022-11-13 22:11:17 +0200

    + working getItem

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,13 +8,13 @@ import { nanoid } from 'nanoid'
 export default function App() {
   
   const [contacts, setContact] = useState(
-    
-    [
+    () => JSON.parse(window.localStorage.getItem('data')) ?? []
+    // [
       // {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
       // {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
       // {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
       // {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-    ]
+    // ]
   )
 
   const[filter, setFilter] = useState('')
@@ -72,12 +72,12 @@ export default function App() {
   }
 
 
-  useEffect(() => {
-    const contactData = localStorage.getItem("data")
-    const parsedUserContact = JSON.parse(contactData)
-    console.log(parsedUserContact)
-    setContact(parsedUserContact)
-  }, []);
+  // useEffect(() => {
+  //   const contactData = localStorage.getItem("data")
+  //   const parsedUserContact = JSON.parse(contactData)
+  //   console.log(parsedUserContact)
+  //   setContact(parsedUserContact)
+  // }, []);
 
 
   useEffect(() => {
